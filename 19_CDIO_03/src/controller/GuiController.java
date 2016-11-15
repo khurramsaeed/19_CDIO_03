c2f2951df595bbd861afe158f6d0a635f11d4238
Swagam <Swagam@192.168.0.16>
2016-11-15 14:49:43 +0100

    GuiController added fields

--- 19_CDIO_03/src/controller/GuiController.java
+++ 19_CDIO_03/src/controller/GuiController.java
@@ -5,28 +5,35 @@ import java.awt.Color;
 import desktop_fields.Field;
 import desktop_fields.Street;
 import desktop_resources.GUI;
+import entity.Cup;
+import entity.Dice;
 
+import entity.Dice;
 public class GuiController {
 	
 	public void GUIField(){
-
+		
+		Dice d1 = new Dice();
+		Dice d2 = new Dice();
+		
+		Cup c1 = new Cup();
+		
 		
 		Field[] fields = new Field[3];
 		
-		fields[0] = new Street.Builder()
-		.setTitle("Anker Engelundsvej")
-		.setRent("600,-")
+		fields[0] = new desktop_fields.Start.Builder()
+		.setTitle("Start")
+		.setDescription("This is the start, you will receive 30000 coins as a starting bonus")
+		.setBgColor(Color.ORANGE)
 		.build();
-		fields[1] = new desktop_fields.Refuge.Builder().setTitle("Fahad").build();
-		fields[2] = new Street.Builder()
-		.setTitle("Din mor")
-		.setRent("4000")
-		.setSubText("Søren var en fed ko")
-		.setBgColor(Color.red)	
+		
+		fields[1] = new desktop_fields.Refuge.Builder()
+		.setTitle("Tribe Encampment")
+		.setDescription("hey")
+		.setBgColor(Color.green)
 		.build();
 		
 		GUI.create(fields);
-		
 		GUI.showMessage("");
 	}
 
