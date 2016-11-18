20f4a59f57f3b7e6e7b8b39d31f869b55f151a43
s165162 <s165162@DTU-CZC6268C7H.win.dtu.dk>
2016-11-18 11:23:26 +0100

    GC Commented

--- 19_CDIO_03/src/controller/GameController.java
+++ 19_CDIO_03/src/controller/GameController.java
@@ -16,8 +16,21 @@ import desktop_fields.*;
 import entity.*;
 
 public class GameController {
-PlayerList player = new PlayerList();
+	PlayerList player = new PlayerList();
 
-Player p = new Player("");
+	Player p = new Player("");
+
+	// LandOnField method
+	
+		//If the given field is a tax field, ask the given player if he wants to pay 10% or a specific amount.
+	
+			//Checks if the given field is a Fleet/Territory/LaborCamp field. (a.k.a is a ownable field).
+	
+			// If field is owned
+
+	// Change current balance with new balance
+	
+	
+	//Checks if the player who had to pay has lost.
 
 }
