440bda9cda1b8cc1ca8b45dc1d59117f29cb2d11
Khurram Saeed Malik <Khurrrmiiii@khurram-macbook.eduroam.wireless.dtu.dk>
2016-11-18 08:13:25 +0100

    Launch updated

--- 19_CDIO_03/src/controller/Launch.java
+++ 19_CDIO_03/src/controller/Launch.java
@@ -9,34 +9,30 @@ import entity.GameBoard;
 import entity.Player;
 import entity.PlayerList;
 
-
 public class Launch {
-	
-	
 
 	public static void main(String[] args) {
-		
-PlayerList PL = new PlayerList();
-PL.addPlayer();
-		
-System.out.println(PL.PlayerListLength());
-		
-GameBoard gb = new GameBoard();
-GuiController gc = new GuiController();
-Player p = new Player("");
+		System.out.println("Hvor mange spiller ønsker at spille?");
+		PlayerList PL = new PlayerList();
 
-Cup c1 = new Cup();
+		PL.addPlayer();
 
-gc.GUIField(gb.getFields(), PL);
+		System.out.println(PL.PlayerListLength());
 
-while(true){
-gc.GUIaddPlayer(PL, p);
-gc.GUIDice(PL, p);
+		GameBoard gb = new GameBoard();
+		GuiController gc = new GuiController();
+		Player p = new Player("");
 
+		Cup c1 = new Cup();
 
-}
-		
+		gc.GUIField(gb.getFields(), PL);
+
+		while (true) {
+			gc.GUIaddPlayer(PL, p);
+			gc.GUIDice(PL, p);
+
+		}
+
 	}
-	
-	
+
 }
